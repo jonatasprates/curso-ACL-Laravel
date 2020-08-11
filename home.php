99ca49adc0f98ecf501d4ab836e18827f5022f09
Jonatas Prates <jonatasluisprates@gmail.com>
2020-08-10 23:24:44 -0300

    subindo aplicação no git

---
+++ home.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <link rel="base" href="http://www.especializati.com.br"/>
+
+        <title>EscolaTI - Cursos Online de Programa&ccedil;&atilde;o e Forma&ccedil;&otilde;oes para a Web</title>
+        <meta name="description" content="EspecializaTi"/>
+
+        <meta name="robots" content="index, follow"/>
+
+        <!--SEO e SMO-->
+<!--
+        <link rel="publisher" href="https://plus.google.com/+EspecializatiBr"/>
+        <link rel="author" href="http://plus.google.com/u/0/114358846277015613664"/>
+        <link rel="canonical" href="http://www.especializati.com.br"/>
+-->
+
+        <meta itemprop="name" content="EspecializaTI - Cursos Online de Programação, ti, php, jQuery, JavaScript, especialização web">
+        <meta itemprop="description" content="EspecializaTi é um portal de ensino a distância, como o objetivo social de formar especialistas!">
+        <meta itemprop="image" content="http://especializati.com.br/assets/portal/imgs/especializati.png">
+        <meta itemprop="url" content="http://especializati.com.br">
+
+        <meta property="og:type" content="article">
+        <meta property="og:title" content="EspecializaTI - Cursos Online de Programação, ti, php, jQuery, JavaScript, especialização web">
+        <meta property="og:site_name" content="EspecializaTi - Torne-se um especialista você também!">
+        <meta property="og:url" content="http://especializati.com.br">
+        <meta property="og:description" content="EspecializaTi é um portal de ensino a distância, como o objetivo social de formar especialistas!">
+        <meta property="fb:app_id" content="100002680058767">
+        <meta property="og:locale" content="pt_BR">
+        <meta property="og:image" content="http://especializati.com.br/assets/portal/imgs/especializati.png">
+        <meta property="article:author" content="https://www.facebook.com/codecarlos">
+        <meta property="article:publisher" content="https://www.facebook.com/especializaTi">
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@especializati">
+        <meta name="twitter:title" content="EspecializaTI - Cursos Online de Programação, ti, php, jQuery, JavaScript, especialização web">
+        <meta name="twitter:description" content="EspecializaTi é um portal de ensino a distância, como o objetivo social de formar especialistas!">
+        <meta name="twitter:image:src" content="http://especializati.com.br/assets/portal/imgs/especializati.png">
+
+	<!--Verificação Google-->
+	<meta name="google-site-verification" content="QnwkK1K3Y0JZXVehH0W8hg71UQ0lry3y1QLiKgBcQts" />
+	
+        <!--Styles-->
+        <link media="all" type="text/css" rel="stylesheet" href="http://especializati.com.br/assets/portal/css/bootstrap.min.css">
+
+        <link media="all" type="text/css" rel="stylesheet" href="http://especializati.com.br/assets/portal/css/especializati.css">
+
+        <link media="all" type="text/css" rel="stylesheet" href="http://especializati.com.br/assets/portal/css/funil.css">
+
+        <link media="all" type="text/css" rel="stylesheet" href="http://especializati.com.br/assets/portal/css/especializati-responsive.css">
+
+
+        <!-- Custom Fonts -->
+        <link media="all" type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+
+        <link rel="icon" type="image/png" href="http://especializati.com.br/assets/imagens/favicon.png">    </head>
+    <body>
+            <!--header topo-->
+<header class="topo">
+    <header class="bg-padrao topo-user">
+        <div class="container">
+            <h1 class="ocuta">EspecializaTI - Cursos Online de Programação, ti, php, jQuery, JavaScript, curso web</h1>
+            <div class="col-md-12">
+                <div class="col-md-6 login">
+                                            <img src="http://especializati.com.br/assets/portal/imgs/icones/logado-especializati.png" title="N&atilde;o Logado" alt="N&atilde;o Logado">
+                        <p>Olá visitante, seja bem vindo(a)!</p>
+                                    </div>
+                <div class="col-md-6 text-right campus">
+                    <a href="http://especializati.com.br/campus" title="Acessar Campus | EspecializaTI" class="texto-branco">
+                        <p>Área do Aluno | EscolaTi</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="logo col-md-3 text-center">
+            <a href="http://especializati.com.br" title="EspecializaTI">
+                <img src="http://especializati.com.br/assets/portal/imgs/logo.png" title="EspecializaTi - Torne-se um especialista voc&ecirc; tamb&eacute;m!" alt="EspecializaTi - Logo">
+            </a>
+        </div>
+        <div class="menu col-md-9">
+            <div class="navbar reset float-left menu" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-especializati">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="clear"></div>
+                <div class="collapse navbar-collapse" id="menu-especializati">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="http://especializati.com.br" title="Home | EspecializaTI">HOME</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#cursos-online" class="dropdown-toggle" data-toggle="dropdown" title="Cursos Online | EspecializaTI">
+                                CURSOS ONLINE <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-web-developer" title="Conhe&ccedil;a o curso Web Developer da especializati">Web Developer</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-de-laravel-5" title="Conhe&ccedil;a o curso Laravel 5.1 da especializati">Laravel 5.1</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-html5-samurai" title="Conhe&ccedil;a o curso HTML5 Samurai da especializati">HTML5 Samurai</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-seo-2016" title="Conhe&ccedil;a o curso SEO 2016 da especializati">SEO 2016</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-php-mvc" title="Conhe&ccedil;a o curso PHP MVC Advanced da especializati">PHP MVC Advanced</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-php-samurai" title="Conhe&ccedil;a o curso PHP Samurai da especializati">PHP Samurai</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-php-pdo" title="Conhe&ccedil;a o curso PDO + RELATIONSHIPS da especializati">PDO + RELATIONSHIPS</a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://especializati.com.br/curso-laravel-5" title="Conhe&ccedil;a o curso Laravel 5.2 da especializati">Laravel 5.2</a>
+                                    </li>
+                                                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#cursos-gratuitos" class="dropdown-toggle" data-toggle="dropdown" title="Cursos Gratuitos Desenvolvimento Web | EspecializaTI">
+                                CURSOS GRÁTIS <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/1/laravel" title="Laravel">Laravel</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/5/seguranca-web" title="Seguran&ccedil;a WEB">Seguran&ccedil;a WEB</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/8/linux" title="Linux">Linux</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/9/curso-laravel5-gratis" title="Laravel 5.1">Laravel 5.1</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/10/curso-gratis-html5-samurai" title="HTML5 Samurai">HTML5 Samurai</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/11/curso-php-composer" title="PHP COMPOSER">PHP COMPOSER</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/12/curso-laravel-5-2" title="Laravel 5.2">Laravel 5.2</a>
+                                </li>
+                                                                <li>
+                                    <a href="http://especializati.com.br/categoria/13/curso-laravel-acl" title="Laravel + ACL">Laravel + ACL</a>
+                                </li>
+                                                            </ul>
+                        </li>
+                        <li>
+                            <a href="http://especializati.com.br/empresa" title="&Agrave; Empresa | EspecializaTI">EMPRESA</a>
+                        </li>
+                        <li>
+                            <a href="http://especializati.com.br/contato" title="Entre em contato | EspecializaTI">CONTATO</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="search">
+                <form method="POST" action="http://especializati.com.br" accept-charset="UTF-8" id="form-search" class="form-seach"><input name="_token" type="hidden" value="VnJIbvAD7fTeGze4z4J6ONUtyJKjheouxb7LQZqh">
+                    <input class="serch-input" id="palavra-pesquisa" placeholder="Encontre o que procura" name="palavra-pesquisa" type="text" value="">
+                    <input class="serch-submit" type="submit" value="">
+                </form>
+            </div>
+        </div><!--menu-->
+    </div><!--container-->
+</header>
+ 
+ 
+ 
+ 
+ 
+  <footer class="col-md-12 text-center copyright">
+            <p>CopyRight &copy; EspecializaTI - Todos os direitos reservados 2016</p>
+        </footer>
+
+ <!-- jQuery -->
+        <script src="http://especializati.com.br/assets/portal/js/jquery.js"></script>
+
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="http://especializati.com.br/assets/portal/js/bootstrap.min.js"></script>
+</body>
+</html>
